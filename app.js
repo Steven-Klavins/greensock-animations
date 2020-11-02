adf508865b5d7777700e4f8432d8017ecde25081
Steven-Klavins <stevenklavins94@gmail.com>
2020-11-02 17:36:36 +0000

    chore: added code for first animation roll

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
-gsap.to(".logo", {duration: 2, x: 300});
+gsap.to(".logo", {duration: 2, rotation: 360, x: 300});
+gsap.to(".logo2", {duration: 2, rotation: 360, x: 600});
+gsap.to(".logo3", {duration: 2, rotation: 360, x: 900});
