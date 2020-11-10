59702ebac2951a8ca35bf46079032718cb94ce6c
Steven-Klavins <stevenklavins94@gmail.com>
2020-11-10 19:37:48 +0000

    chore: added rotate animation

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 gsap.to(".logo", {duration: 2, rotation: 360, x: 300});
 gsap.to(".logo2", {duration: 2, rotation: 360, x: 600});
 gsap.to(".logo3", {duration: 2, rotation: 360, x: 900});
+
+gsap.to(".rotate", {duration: 10, rotation: 360});
