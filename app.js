24f3be6114278b68c5822b24eccfdbc267b1655e
Steven-Klavins <stevenklavins94@gmail.com>
2020-11-10 20:14:25 +0000

    chore: added stagger animation

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+gsap.from(".staggerlogos", {duration: 1, opacity: 0, y: 150, stagger: 0.25 });
+ 
 gsap.to(".logo", {duration: 2, rotation: 360, x: 300});
 gsap.to(".logo2", {duration: 2, rotation: 360, x: 600});
 gsap.to(".logo3", {duration: 2, rotation: 360, x: 900});
